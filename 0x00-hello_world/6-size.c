de67b34a65dc3f646941f79d10cf082311c58d6c
Alaa <aemm7682@gmail.com>
2023-07-14 19:19:17 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program to check the size of variable in c
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char", sizeof(Char));
+	printf("Size of  a int", sizeof(int));
+	printf("Size of a long int", sizeof(long int));
+	printf("Size of a long long int", sizeof(long long int));
+	 printf("Size of a float", sizeof(float));
+	return (0);
+}
