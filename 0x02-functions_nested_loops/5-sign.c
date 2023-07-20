a2d256af91e85ab1b19551d2071e265bd54af994
Alaa <aemm7682@gmail.com>
2023-07-19 18:05:07 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,12 +9,9 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		printf("+");
-	return (1);
+		return (+1);
 	if (n < 0)
-		printf("-");
-	return (-1);
+		return (-1);
 	if (n == 0)
-		printf("0");
-	return (0);
+		return (0);
 }
