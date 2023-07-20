3091318b3c553ce9ce45209b4851afe5315ad105
Alaa <aemm7682@gmail.com>
2023-07-19 19:12:27 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - is function to print last digit
+ *
+ * @z: is the input to function
+ *
+ * Return: is 0 success
+ */
+int print_last_digit(int z)
+{
+	int last_dig;
+
+	last_deg = z % 10;
+	_putchar(last_deg + '0');
+	return (0);
+}
+
