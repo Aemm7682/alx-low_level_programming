ab170059b66bb38426828b7c2089354c19a0cce1
Alaa <aemm7682@gmail.com>
2023-07-19 18:35:58 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - is function to give only positive value
+ *
+ * @int: is the input to function
+ *
+ * Return: is 1 if + and -1 if - else 0
+ */
+int _abs(int)
+{
+	int a;
+
+	int b = abs(a);
+
+	return (b);
+}
