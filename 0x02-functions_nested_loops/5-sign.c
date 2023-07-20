6f2479304a8534741555b6999053e71fb9abd16b
Alaa <aemm7682@gmail.com>
2023-07-19 17:53:15 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - is function to check if the input sign
+ *
+ * @n: is the input to function
+ *
+ * Return: is 1 if + and -1 if - else 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		printf("+");
+	return (1);
+	if (n < 0)
+		printf("-");
+	return (-1);
+	else
+		print("0");
+	return (0);
+}
