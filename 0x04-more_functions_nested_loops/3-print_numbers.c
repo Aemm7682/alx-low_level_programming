8e0497d21f77b892cf434f1796171c08c56c373f
Alaa <aemm7682@gmail.com>
2023-07-23 01:51:05 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - function to check uppercase
+ *
+ * @c: is input to check
+ *
+ *Return: 1 if uppercase and 0 if not
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i == 0 ; i <= 9 ; i++)
+		_putchar(i + 48);
+	_putchar('\n');
+	return (0);
+}
