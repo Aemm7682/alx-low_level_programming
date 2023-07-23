90a973306f08985a1a1a137a728e46deb39875e2
Alaa <aemm7682@gmail.com>
2023-07-23 01:33:25 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - function to check uppercase
+ *
+ * @a: is input to check
+ *
+ * @b: is input to check
+ *
+ *Return: mul
+ *
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (sum);
+}
