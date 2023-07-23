ad371ba79571b75319d81094e74844bd01cd99c6
Alaa <aemm7682@gmail.com>
2023-07-23 04:56:58 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j <= 14 ; j++)
 		{
 			if (j == 10 || j <= 14)
 			{
-				_putchar('1' + j + 48);
+				_putchar(1);
 			}
 			else
 			       _putchar(j + 48);
