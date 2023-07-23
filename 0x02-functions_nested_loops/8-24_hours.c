81333c0e8264a6279e021f61a9a32cd9da181aeb
Alaa <aemm7682@gmail.com>
2023-07-23 00:07:48 -0700

    0-isupper.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_last_digit - is function to print last digit
+ *
+ * @z: is the input to function
+ *
+ * Return: is 0 success
+ */
+void jack_bauer(void)
+{
+	int hr;
+	int min;
+	
+	for (hr = 0 ; hr <= 23; hr++)
+	{
+		for (min = 0 ; min <= 59 ; min++)
+		{
+			_putchar(hr + 48);
+			_putchar(':');
+			_putchar(min + 48);
+		}
+	}
+	return (0);
+
+}
