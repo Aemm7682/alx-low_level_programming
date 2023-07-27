cdb84ec2c057498d1a0071cde405671e33ec44fc
Alaa <aemm7682@gmail.com>
2023-07-27 02:49:05 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strncat - a function that concatenates two string
+ *
+ * @dest: variable
+ *
+ * @src: variable
+ *
+ * @n: variable
+ *
+ * Return: dest is success
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
