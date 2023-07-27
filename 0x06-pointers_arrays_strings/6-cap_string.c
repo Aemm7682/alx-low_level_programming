a4acf613650b0adf95f225ff6190fa7905f083ce
Alaa <aemm7682@gmail.com>
2023-07-27 15:03:45 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * d - aux function
+ *
+ * @c: variable
+ *
+ * Return: return 1 if true and 0 if false
+ */
+char d(char c)
+{
+	int i;
+	char e[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0 ; e[i] ; i++)
+		if (c == e[i])
+			return (1);
+	return (0);
+}
+/**
+ * cap_string - cap string
+ *
+ * @s: variable
+ *
+ * Return: return s
+ */
+char *cap_string(char *s)
+{
+	int xzo = 1;
+	int j;
+
+	for (j = 0 ; s[i] ; i++)
+	{
+		if (xzo && s[j] >= 'a' && s[j] <= 'z')
+			s[j] -= 32;
+		xzo = d(s[j]);
+	}
+	return (s);
+}
