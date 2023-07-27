995d7ca9d955fb91c62f7cb6d3a8cd7266b6dbd3
Alaa <aemm7682@gmail.com>
2023-07-27 15:59:17 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,20 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char let[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+	for (i = 0 ; s[i] ; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0 ; let[j] ; j++)
+		{
+			if (s[i] == let[j])
+			{
+				s[i] = num[j];
+				break;
+			}
+		}
 	}
 	return (s);
 }
