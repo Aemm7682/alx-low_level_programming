f4c23b87f818be9690be2ee021c9918c81fe1573
Alaa <aemm7682@gmail.com>
2023-08-05 19:13:56 -0700

    project 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: variable
+ * @argv: array
+ * Return: return
+ */
+int main(int argc, char const *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
