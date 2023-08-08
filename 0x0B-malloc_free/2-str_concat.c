81df69202479a07172cb3b727c76afbc7f9bd41d
Alaa <aemm7682@gmail.com>
2023-08-08 03:24:15 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,6 +8,8 @@ int _strlen(char *s)
 {
 	int size = 0;
 
+	if (s == NULL)
+		return (0);
 	for (; s[size] != '\0' ; size++)
 		;
 	return (size);
@@ -23,13 +25,9 @@ char *str_concat(char *s1, char *s2)
 	int size1, size2, i;
 	char *m;
 
-	if (s1 == NULL)
-		s1 = '\0';
-	if (s2 == NULL)
-		s2 = '\0';
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
-	m = malloc((size1 + size2) * sizeof(char) + 1);
+	m = malloc((size1 + size2 + 1) * sizeof(char));
 	if (m == 0)
 	{
 		return (0);
