c09b20597a7833bacd6871d64e9ac1df6ad9cc8c
Alaa <aemm7682@gmail.com>
2023-08-11 17:22:20 -0700

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * *memset - function
+ * @s: pointer to put constant
+ * @b: constant
+ * @n: max byte use
+ * Return: ptr
+ */
+char *memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+	return (ptr);
+}
+/**
+ * *_calloc - put mem in array by malloc
+ * @nmemb: array length
+ * @size: size of each element
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	m = malloc(nmemb * sizeof(int));
+	if (m == 0)
+		return (NULL);
+	memset(m, 0, nmemb * sizeof(int));
+	return (m);
+}
