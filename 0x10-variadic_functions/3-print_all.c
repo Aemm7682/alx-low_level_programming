b26a7a6c7f3d72bedc52958559cbcbde72156c60
Alaa <aemm7682@gmail.com>
2023-08-26 09:46:06 -0700

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -50,11 +50,11 @@ void print_all(const char * const format, ...)
 	char *separator = "";
 	va_list ap;
 	token_t tokens[] = {
-		{"c", format_char};
-		{"i", format_int};
-		{"f", format_float};
-		{"s", format_string};
-		{NULL, NULL};
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL},
 	};
 
 	va_start(ap, format);
