2670f53996481debaec363894d360f6799fc638b
Alaa Eldin <aemm7682@gmail.com>
2023-09-30 08:46:49 -0700

    sqrt program without math lib

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,33 @@
 #include "main.h"
 /**
- * aux_sqrt_recursion - auxiliary fun to get the squrt
- * @min: minmum value
+ * _sqrt_recursion - the main function that call the aux to solve
+ * @n: the number to get sqrt
+ * @min: minmum
+ * @max: max
+ * Return: return
+ */
+int aux_sqrt_recursion(int n, int min, int max);
+int _sqrt_recursion(int n)
+{
+	return (aux_sqrt_recursion(n, 0, n));
+}
+/**
+ * aux_sqrt_recursion - aux function to get sqrt
+ * @n: variable
+ * @min: min value
  * @max: maximum value
- * Return: depending in the output
+ * Return: to return
  */
 int aux_sqrt_recursion(int n, int min, int max)
 {
-	int mid = (min + max)/ 2;
+	int mid = (min + max) / 2;
+
 	if (max < min)
 		return (-1);
-	if (mid * mid == n)
+	else if ((mid * mid) == n)
 		return (mid);
-	else if (mid * mid < n)
-	{
+	else if ((mid * mid) < n)
 		return (aux_sqrt_recursion(n, mid + 1, max));
-	}
-	else if (mid * mid > n)
-        {
-                return (aux_sqrt_recursion(n, min, mid - 1));
-        }
-}
-/**
- * _sqrt_recursion - the main function that call the aux to solve
- * @n: the number to get sqrt
- */
-int _sqrt_recursion(int n)
-{
-	aux_sqrt_recursion(n,0 ,n);
+	else
+		return (aux_sqrt_recursion(n, min, mid - 1));
 }
