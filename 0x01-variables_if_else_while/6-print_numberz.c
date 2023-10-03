7a80b02e9235bd95534456fb09b127de8fddc14d
Alaa Eldin <aemm7682@gmail.com>
2023-10-03 06:56:19 -0700

    1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program to write alpheb
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char x = '0';
+
+	for (; x <= '9' ; x++)
+	{
+		putchar(x);
+		putchar('\n');
+	}
+	return (0);
+}
