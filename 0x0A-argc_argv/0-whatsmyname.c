710800a2cbf1115d23d1f0dbf92bd678070ea669
Alaa <aemm7682@gmail.com>
2023-10-27 15:52:02 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is a function to get argc and argv
+ * @argc: is the number of word include the program name
+ * @argv: is the
+ * Return: return 0
+ */
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
