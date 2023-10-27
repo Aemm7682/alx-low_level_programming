96d4a2cccd21a8b29e58560ea68659c5c4db6227
Alaa <aemm7682@gmail.com>
2023-10-27 16:34:01 -0700

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program should print a number, followed by a new line
+ * @argc: variable
+ * @argv: variable
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
