7eac760ac1aa48c127e6c450dc17420ac88849c1
Alaa <aemm7682@gmail.com>
2023-10-31 08:52:17 -0700

    a function that returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * _strlength - function to get the pointer length
+ * @a: is pointer for the auxiliary function
+ * Return: to return length
+ */
+int _strlength(char *a)
+{
+	int i = 0;
+
+	for (; *a ; a++, i++)
+		;
+	return (i);
+}
+/**
  * _strdup - is main function to copy pointer
  * @str: is the main pointer
  * Return: the pointer x
