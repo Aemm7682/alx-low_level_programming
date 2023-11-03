56c0bda24196b3a4d013df27b2979c025d4b9f8f
Alaa <aemm7682@gmail.com>
2023-11-03 14:50:48 -0700

    repaired

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,14 +10,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
 	void *z;
-	char *x;
-	char *y;
 
-	x = ptr;
-	y = z;
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 ||ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
@@ -35,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		if (z == NULL)
 			return (NULL);
 		for (i = 0 ; i < old_size && i < new_size ; i++)
-			y[i] = x[i];
+			*((char *)z + i) = *((char *)ptr + i);
 		free(ptr);
 	}
 	return (z);
