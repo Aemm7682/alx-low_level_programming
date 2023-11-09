09604308b65cda85014a10d877c71c9c3920712a
Alaa <aemm7682@gmail.com>
2023-11-08 17:22:04 -0800

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+/**
+ * main - entry point
+ * @argc: number of argment
+ * Return: 0
+ */
+int main(int argc, char **argc)
+{
+	int (*op_funs)(int, int);
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_funs = get_op_func(argv[2]);
+	if (!op_funs)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100)
+	}
+	printf("%d\n", op_funs(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - function to add two number
+ * @a: integer
+ * @b: input
+ * Return: value
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function to add two number
+ * @a: integer
+ * @b: input
+ * Return: value
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function to add two number
+ * @a: integer
+ * @b: input
+ * Return: value
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function to add two number
+ * @a: integer
+ * @b: input
+ * Return: value
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function to add two number
+ * @a: integer
+ * @b: input
+ * Return: value
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
