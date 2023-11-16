17493ce8544de8e8a81b023b04fdbe30515f3deb
Alaa <aemm7682@gmail.com>
2023-11-16 12:52:39 -0800

    zero

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * _strlen - to return the length of the string
+ * @s: the string that need to checked
+ * Return: to return the legth
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * print_list - print liked list
+ * @h: pointer to first node
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
