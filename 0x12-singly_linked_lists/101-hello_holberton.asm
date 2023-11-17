32b7a64e59b565fe9c9d4115da731bfd663b57ff
Alaa <aemm7682@gmail.com>
2023-11-16 23:16:36 -0800

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,19 @@
-section		.text
-	extern 	printf
-	global  main
-main:
-	mov		edi, msg
-	mov		eax, 0
-	call	printf
+section .data
+    hello db 'Hello, Holberton', 10, 0
 
-section		.data
-	msg db	'Hello, holberton', 0xa, 0
+section .text
+    global main
+    extern printf
+
+main:
+    ; Prepare the arguments for printf
+    mov rdi, hello
+    xor rax, rax
+
+    ; Call printf
+    call printf
+
+    ; Return from main
+    mov eax, 0x60
+    xor edi, edi
+    syscall
