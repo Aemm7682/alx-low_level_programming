ab199ce8e94f8b9f6f3574e652a1291cbab50a5e
Alaa <aemm7682@gmail.com>
2023-12-01 18:27:03 -0800

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@
  */
 int main(int ac, char **av)
 {
-	int from_fd = 0, t_fd = 0;
+	int from_fd = 0, to_fd = 0;
 	ssize_t b;
 	char buf[READ_BUF_SIZE];
 
@@ -24,12 +24,12 @@ int main(int ac, char **av)
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	to_fd = iopen(av[2], O_WRONLY | O_CREAT | O_TRUNC | PERMISSIONS);
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | PERMISSIONS);
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
 		if (write(to_fd, buf, b) != b)
-			dprintf(STDERR_FILENO, ERR_BOWRITE, av[2]), exit(99);
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[2]), exit(98);
 	from_fd = close(from_fd);
