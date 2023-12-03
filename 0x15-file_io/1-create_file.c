1d591ccea8621f95b944cfbbe18feecac37177e0
Alaa <aemm7682@gmail.com>
2023-12-02 18:42:47 -0800

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,10 +8,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes = 0, len = strlen(text_content);
+	ssize_t bytes = 0, len = 0;
 
 	if (!filename)
 		return (-1);
+	if (text_content)
+		len = strlen(text_content);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
