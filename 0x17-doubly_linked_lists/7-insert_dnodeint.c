bd54bb85ba4709805cbecd62be5ce85bb9d3f58a
Alaa <aemm7682@gmail.com>
2024-01-15 00:48:15 -0800

    7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert a new node in list at given position
+ * @h: pointer the head of the list
+ * @idx: poision to insert the new node
+ * @n: integer for new node
+ * Return: if fail give NULL other new node address
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = *h, *new_node;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	for (; idx != 1; idx--)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);
+}
