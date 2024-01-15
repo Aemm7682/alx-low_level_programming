579d202c28cac417695c6c524ed2efdc4564c5d5
Alaa <aemm7682@gmail.com>
2024-01-15 02:25:03 -0800

    8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete node from list
+ * @head: pointer to the first node
+ * @index: index of node to be deleted
+ * Return: on succes -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+
+	if (*head == NULL)
+		return (-1);
+	for (; index != 0; index--)
+	{
+		if (temp == NULL)
+			return (-1);
+		temp = temp->next;
+	}
+	if (temp == *head)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		temp->prev->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = temp->prev;
+	}
+	free(temp);
+	return (1);
+}
