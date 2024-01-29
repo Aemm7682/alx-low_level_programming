ae3418b81eef6cd3d13d03c127ddd55b7e3e95b7
Alaa <aemm7682@gmail.com>
2024-01-29 04:59:12 -0800

    correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,11 @@ void more_numbers(void)
 			if (j >= 10)
 			{
 				_putchar('1');
+				_putchar('0' + j % 10);
 			}
 			else
-			       _putchar('0' + j % 10);
+			       _putchar('0' + j);
 		}
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
