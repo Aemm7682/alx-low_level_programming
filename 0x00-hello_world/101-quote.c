a68e1b694ee1e203b1076bdaeb6c6c7bdfdc304b
Alaa <aemm7682@gmail.com>
2024-02-05 20:43:22 -0800

    intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: A C program to print by puts not printf
+ *
+ * Return: Always 1 (success)
+ */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
