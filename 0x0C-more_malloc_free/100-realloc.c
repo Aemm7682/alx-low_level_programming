639985a257001c4dc4cf25107e90bd1f10a794c9
Alaa <aemm7682@gmail.com>
2024-03-02 23:29:06 -0800

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		z = malloc(new_size);
-		if ( z == NULL)
+		if (z == NULL)
 			return (NULL);
 		return (z);
 	}
