825eab1e704c40b611360e37da219bae28adc78b
Alaa <aemm7682@gmail.com>
2024-05-11 21:22:08 +0300

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,10 +9,11 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+	hash_node_t *new_node;
 	hash_node_t *temp;
 
 	index = key_index((unsigned char *)key, ht->size);
-	if (!ht || !key || !value)
+	if (!ht || !key || strlen(key) == 0 || ht->size == 0 || !value)
 		return (0);
 	temp = ht->array[index];
 	while (temp)
@@ -27,5 +28,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-	return (0);
+	new_node = malloc(sizeof(hash_node_t *));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
